fb9456c1320e368b18ae3610ac66ce44281ad1df
Yuting Wan <30644585+TheBigRedOne@users.noreply.github.com>
2025-01-30 12:38:40 +0000

    Update hello_server_win.c for Lab2

--- hello_server_win.c
+++ hello_server_win.c
@@ -1,80 +1,95 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <stdio.h>
+#include <string.h>
 
 #pragma comment(lib, "Ws2_32.lib") // 链接 Winsock 库
 
 #define BUFLEN 1500
-
-#ifdef SERVER
+#define HTTP_PORT 80
 
 int main(void) {
     WSADATA wsaData;
-    SOCKET fd, conn_fd;
-    struct sockaddr_in addr, conn_addr;
-    int conn_addr_len;
+    SOCKET server_fd, client_fd;
+    struct sockaddr_in server_addr, client_addr;
+    int client_addr_len;
     char buf[BUFLEN];
     int flags = 0;
     int rlen;
 
     // 初始化 Winsock
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        perror("WSAStartup failed");
+        fprintf(stderr, "WSAStartup failed: %d\n", WSAGetLastError());
         return 1;
     }
 
     // 创建 TCP/IP 套接字
-    fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (fd == INVALID_SOCKET) {
-        perror("Unable to create socket");
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == INVALID_SOCKET) {
+        fprintf(stderr, "Unable to create socket: %d\n", WSAGetLastError());
         WSACleanup();
         return 1;
     }
 
-    // 绑定到端口 5001
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(5001);
-    addr.sin_addr.s_addr = INADDR_ANY;
+    // 绑定到端口 80
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(HTTP_PORT);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(fd, (struct sockaddr*)&addr, sizeof(addr)) == SOCKET_ERROR) {
-        perror("Unable to bind to port");
-        closesocket(fd);
+    if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
+        fprintf(stderr, "Unable to bind to port: %d\n", WSAGetLastError());
+        closesocket(server_fd);
         WSACleanup();
         return 1;
     }
 
     // 监听连接
-    if (listen(fd, 1) == SOCKET_ERROR) {
-        perror("Unable to listen for connection");
-        closesocket(fd);
+    if (listen(server_fd, 5) == SOCKET_ERROR) {
+        fprintf(stderr, "Unable to listen for connection: %d\n", WSAGetLastError());
+        closesocket(server_fd);
         WSACleanup();
         return 1;
     }
 
-    // 接受第一个连接
-    conn_addr_len = sizeof(conn_addr);
-    conn_fd = accept(fd, (struct sockaddr*)&conn_addr, &conn_addr_len);
-    if (conn_fd == INVALID_SOCKET) {
-        perror("Unable to accept connection");
-        closesocket(fd);
-        WSACleanup();
-        return 1;
-    }
+    printf("HTTP Server running on port %d...\n", HTTP_PORT);
 
-    // 接收数据
-    while ((rlen = recv(conn_fd, buf, BUFLEN, flags)) > 0) {
-        for (int i = 0; i < rlen; i++) {
-            printf("%c", buf[i]);
+    // 接受连接并处理请求
+    while (1) {
+        client_addr_len = sizeof(client_addr);
+        client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_addr_len);
+        if (client_fd == INVALID_SOCKET) {
+            fprintf(stderr, "Unable to accept connection: %d\n", WSAGetLastError());
+            continue; // 继续接受下一个连接
         }
-        printf("\n");
+
+        printf("Client connected...\n");
+
+        // 读取 HTTP 请求
+        memset(buf, 0, BUFLEN);
+        rlen = recv(client_fd, buf, BUFLEN - 1, flags);
+        if (rlen > 0) {
+            buf[rlen] = '\0';  // 确保是 C 字符串
+            printf("Received request:\n%s\n", buf);
+
+            // 发送 HTTP 响应
+            const char* http_response =
+                "HTTP/1.1 200 OK\r\n"
+                "Content-Type: text/html\r\n"
+                "Connection: close\r\n"
+                "\r\n"
+                "<html><body><h1>Hello, World!</h1></body></html>\r\n";
+
+            send(client_fd, http_response, (int)strlen(http_response), 0);
+        }
+
+        // 关闭连接
+        closesocket(client_fd);
     }
 
-    // 关闭连接和清理
-    closesocket(conn_fd);
-    closesocket(fd);
+    // 关闭服务器
+    closesocket(server_fd);
     WSACleanup();
 
     return 0;
 }
 
-#endif // SERVER
